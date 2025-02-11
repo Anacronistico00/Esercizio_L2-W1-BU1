262cffce3a1cb057408d3bb8727813e4aa51b41e
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-02-11 13:38:01 +0100

    Piccole modifiche

--- Esercizio_L2-W1-BU1/Program.cs
+++ Esercizio_L2-W1-BU1/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Esercizio_L2_W1_BU1.Models;
 
 Persona persona1 = new Persona("Vittorio", "Turiaci", 24);
+Console.WriteLine("Dettagli Singoli persona1:");
 Console.WriteLine("Nome: " + persona1.GetNome());
 Console.WriteLine("Cognome: " + persona1.GetCognome());
 Console.WriteLine("Età: " + persona1.GetEta());
-Console.WriteLine("Dettagli: " + persona1.GetDettagli());
+Console.WriteLine("-----------------");
+Console.WriteLine("Dettagli Completi: ");
+Console.WriteLine(persona1.GetDettagli());
+
+Console.ReadLine(); // Per mantenere aperta la console
