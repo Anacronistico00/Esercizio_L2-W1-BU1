2b54b6b6e27fb7a3c7f05b3451c72ab79da85e5c
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-02-11 13:28:55 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio_L2-W1-BU1/Models/Persona.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Esercizio_L2_W1_BU1.Models
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+        public string GetCognome()
+        {
+            return Cognome;
+
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta} anni";
+        }
+
+    }
+}
---
+++ Esercizio_L2-W1-BU1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Esercizio_L2_W1_BU1.Models;
+
+Persona persona1 = new Persona("Vittorio", "Turiaci", 24);
+Console.WriteLine("Nome: " + persona1.GetNome());
+Console.WriteLine("Cognome: " + persona1.GetCognome());
+Console.WriteLine("Età: " + persona1.GetEta());
+Console.WriteLine("Dettagli: " + persona1.GetDettagli());
